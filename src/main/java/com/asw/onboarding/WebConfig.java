0670c9dbc7ed27fe62a12bc99331c83ac81f0e1e
alphaK11 <keylor.cortes.cascante@est.una.ac.cr>
2025-06-07 15:06:24 -0600

    Configuración CORS para frontend en Vercel

--- src/main/java/com/asw/onboarding/WebConfig.java
+++ src/main/java/com/asw/onboarding/WebConfig.java
@@ -14,8 +14,11 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://asw-frontend-cg0i8yfqk-alphak03s-projects.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
         };
